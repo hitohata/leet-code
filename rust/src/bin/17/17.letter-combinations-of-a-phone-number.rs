4b60306f7dd1848a5c86b450ecdd3d24ae6fe14a
hirohat <hirohatatro@gmail.com>
2023-08-18 16:56:21 -0700

    wip

---
+++ rust/src/bin/17/17.letter-combinations-of-a-phone-number.rs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=17 lang=rust
+ *
+ * [17] Letter Combinations of a Phone Number
+ */
+
+// @lc code=start
+impl Solution {
+
+    const map: [&str; 10] = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+    pub fn letter_combinations(digits: String) -> Vec<String> {
+        let dig_arr = digits;
+    }
+}
+// @lc cod=end
