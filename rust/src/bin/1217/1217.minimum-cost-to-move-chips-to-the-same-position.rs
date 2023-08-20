280bcccdb621d03fb9a84b4794daa8936fe04b1f
hirohat <hirohatatro@gmail.com>
2023-08-20 11:59:01 -0700

    rust complete

--- rust/src/bin/1217/1217.minimum-cost-to-move-chips-to-the-same-position.rs
+++ rust/src/bin/1217/1217.minimum-cost-to-move-chips-to-the-same-position.rs
@@ -8,58 +8,17 @@
 // @lc code=start
 impl Solution {
     pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
-
-        use std::collections::HashMap;
-
-        let mut hash: HashMap<i32, usize> = HashMap::new();
-
-        let mut cost = 0;
-
+        let mut even = 0;
+        let mut odd = 0;
         position.iter().for_each(|el| {
-            let cur = hash.get(el);
-            match hash.get_mut(el) {
-                Some(val) => {
-                    *val += 1
-                },
-                None => {
-                    hash.insert(el.to_owned(), 1);
-                }
-            }
-        });
-
-        fn target_is_odd(map: HashMap<i32, usize>) -> bool {
-            let mut frec = 0;
-            let mut max = 0;
-
-            map.iter().for_each(|el| {
-                // el.1 is frequency
-                if el.1.to_owned() > frec {
-                    // el.0 is number of position.
-                    max = el.0.to_owned();
-                }
-            });
-
-            if max % 2 == 0 {
-                true
+            if el.to_owned() % 2 == 0 {
+                even += 1;
             } else {
-                false
-            }
-        }
-
-        let distinction_odd = target_is_odd(hash);
-
-        position.iter().for_each(|el| {
-
-            let target = el.to_owned();
-            let target_odd = if target % 2 == 0 { true } else { false };
-
-            if distinction_odd == target_odd {
-                cost += 1;
+                odd += 1;
             }
         });
 
-        cost
-
+        std::cmp::min(even, odd)
     }
 }
 // @lc code=end
