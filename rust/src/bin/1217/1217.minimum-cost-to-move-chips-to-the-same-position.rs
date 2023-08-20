b5620225109168afad3f837a8bb9106dff5d07a5
hirohat <hirohatatro@gmail.com>
2023-08-20 10:56:56 -0700

    create a rust env

---
+++ rust/src/bin/1217/1217.minimum-cost-to-move-chips-to-the-same-position.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1217 lang=rust
+ *
+ * [1217] Minimum Cost to Move Chips to The Same Position
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
+        
+    }
+}
+// @lc code=end
+
