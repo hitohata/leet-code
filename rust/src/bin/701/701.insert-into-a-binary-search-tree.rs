2064204e8b4a580b22198af113de105a4bcc3c59
hirohata <hirohatatro@gmail.com>
2023-09-19 22:59:47 -0700

    solved by rust

--- rust/src/bin/701/701.insert-into-a-binary-search-tree.rs
+++ rust/src/bin/701/701.insert-into-a-binary-search-tree.rs
@@ -6,41 +6,39 @@
 
 // @lc code=start
 // Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
-}
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
 
-impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
-    }
-  }
-}
-use std::borrow::BorrowMut;
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
 use std::rc::Rc;
 use std::cell::RefCell;
+type Node = Option<Rc<RefCell<TreeNode>>>;
 impl Solution {
-    pub fn insert_into_bst(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-      Self::insertion(root, val)
-    }
+    pub fn insert_into_bst(root: Node, val: i32) -> Node {
 
-    fn insertion(root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-      return match root {
-        None => { Some(Rc::new(RefCell::new(TreeNode::new(val))))},
+      match root.clone() {
+        None => { return Some(Rc::new(RefCell::new(TreeNode::new(val))))},
         Some(tree) => {
-          if tree.borrow().val > val {
-            tree.borrow_mut().left = Self::insertion(tree.borrow().left.clone(), val);
-          } else if val > tree.borrow().val {
-            tree.borrow_mut().right = Self::insertion(tree.borrow().right.clone(), val);
+          let mut tree_bor = tree.borrow_mut();
+          if tree_bor.val > val {
+            tree_bor.left = Self::insert_into_bst(tree_bor.left.clone(), val);
+          } else if val > tree_bor.val {
+            tree_bor.right = Self::insert_into_bst(tree_bor.right.clone(), val);
           }
-          root
+          return root.clone()
         }
       }
     }
