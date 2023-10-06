d37ed288ffde323a0d200938b9556fbf0d4846c2
hirohata <hirohatatro@gmail.com>
2023-10-05 23:05:28 -0700

    update logic

--- rust/src/bin/62/62.unique-paths.rs
+++ rust/src/bin/62/62.unique-paths.rs
@@ -7,17 +7,17 @@
 // @lc code=start
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let mut grid = vec![vec![1; n as usize]; m as usize];
+        let mut grid = vec![1; n as usize];
 
 
         for i in 1..(m as usize) {
             for j in 1..(n as usize) {
-                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                grid[j] = grid[j - 1] + grid[j];
 
             };
         }
 
-        grid[(m - 1) as usize][(n - 1) as usize]
+        grid[(n - 1) as usize]
     }
 }
 // @lc code=end
--- typescript/62/62.unique-paths.ts
+++ typescript/62/62.unique-paths.ts
@@ -6,15 +6,15 @@
 
 // @lc code=start
 function uniquePaths(m: number, n: number): number {
-    const grid = new Array(m).fill(new Array(n).fill(1));
+    const grid = new Array(n).fill(1);
 
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
-            grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            grid[j] = grid[j] + grid[j - 1];
         }
     }
 
-    return grid[m - 1][n - 1]
+    return grid[n - 1]
 
 };
 // @lc code=end
