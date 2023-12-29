ddc1c1e3f9e342dc12fc4b41690431ba41c4f08b
hirohata <hirohatatro@gmail.com>
2023-12-28 22:49:52 -0800

    added a problem

---
+++ rust/src/bin/1431/1431.kids-with-the-greatest-number-of-candies.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1431 lang=rust
+ *
+ * [1431] Kids With the Greatest Number of Candies
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
+        
+    }
+}
+// @lc code=end
+
