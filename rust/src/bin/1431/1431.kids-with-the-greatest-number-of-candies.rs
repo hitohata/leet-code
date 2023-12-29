49496ac62c126c581c4553f73980dd5948181464
hirohata <hirohatatro@gmail.com>
2023-12-28 22:59:54 -0800

    solved

--- rust/src/bin/1431/1431.kids-with-the-greatest-number-of-candies.rs
+++ rust/src/bin/1431/1431.kids-with-the-greatest-number-of-candies.rs
@@ -7,7 +7,17 @@
 // @lc code=start
 impl Solution {
     pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
-        
+        let mut result = vec![false; candies.len()];
+
+        let max = candies.iter().max().unwrap().to_owned();
+
+        for (index, candy) in candies.iter().enumerate() {
+            if candy + extra_candies >= max {
+                result[index] = true
+            }
+        }
+
+        result
     }
 }
 // @lc code=end
