16614778fae6ad49722df1be0888ca8016c48c71
hirohata <hirohatatro@gmail.com>
2024-01-10 10:51:03 -0800

    cool solution, but slow

--- rust/src/bin/605/605.can-place-flowers.rs
+++ rust/src/bin/605/605.can-place-flowers.rs
@@ -8,51 +8,13 @@
 impl Solution {
     pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
         let mut bed = flowerbed.clone();
-        let mut number_can_be_flowered: i32 = 0;
 
-        if bed.len() == 1 {
-            return if bed[0] == 0 {
-                return n <= 1;
-            } else {
-                return n == 0;
-            }
-        }
+        bed.push(0);
+        bed.insert(0, 0);
 
-        if bed.len() == 2 {
-            if bed[0] == 1 || bed[1] == 1 {
-                return n == 0;
-            } else {
-                return n <= 1;
-            }
-        }
+        let total = bed.split(|&f| f == 1).map(|f| (f.len() - 1) / 2).sum::<usize>();
 
-        for i in 0..bed.len() {
-            if flowerbed[i] == 1 {
-                continue;
-            };
-
-            if i == 0 {
-                if bed[i] == 0 && bed[i + 1] == 0 {
-                    bed[i] = 1;
-                    number_can_be_flowered += 1;
-                }
-                continue;
-            }
-
-            if i == bed.len() - 1 {
-                if bed[i - 1] == 0 && bed[i] == 0 {
-                    number_can_be_flowered += 1;
-                }
-                continue;
-            }
-
-            if bed[i - 1] == 0 && bed[i + 1] == 0 {
-                bed[i] = 1;
-                number_can_be_flowered += 1;
-            };
-        };
-
-        number_can_be_flowered >= n
+        total >= (n as usize)
     }
 }
 // @lc code=end
