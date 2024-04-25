6eb34ca4930aa263a6d87503a94bacbf0dccfe05
hirohata <43802663+hitohata@users.noreply.github.com>
2024-04-25 03:44:49 -0700

    2073

--- javascript/2073.time-needed-to-buy-tickets/2073.time-needed-to-buy-tickets.js
+++ javascript/2073.time-needed-to-buy-tickets/2073.time-needed-to-buy-tickets.js
@@ -11,7 +11,19 @@
  * @return {number}
  */
 var timeRequiredToBuy = function(tickets, k) {
-    
+    const a = "a".charCodeAt(0);
+    const z = "z".charCodeAt(0);
+
+    const dp = new Array(26).fill(0);
+
+    for (i = 0; i < s.length; i++) {
+        let max = 0;
+        for (j = Math.max(a, s.charCodeAt(i) - k); j <= Math.min(z, s.charCodeAt(i) + k); j++) {
+            max = Math.max(max, dp[j - a]);
+        }
+        dp[s.charCodeAt(i) - a] = max + 1
+    }
+
+    return Math.max(...dp)
 };
 // @lc code=end
-
