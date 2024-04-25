509d34c331edf2e8e104a2a3f6c60183777f7df7
hirohata <43802663+hitohata@users.noreply.github.com>
2024-04-25 03:42:35 -0700

    2743

---
+++ javascript/2073.time-needed-to-buy-tickets/2073.time-needed-to-buy-tickets.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=2073 lang=javascript
+ *
+ * [2073] Time Needed to Buy Tickets
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    
+};
+// @lc code=end
+
