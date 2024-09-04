c931b0d464c20763f567d5cc422c746994769bc4
hirohata <hirohatatro@gmail.com>
2024-09-04 11:19:52 -0700

    updated

--- python/125.valid-palindrome/125.valid-palindrome.py
+++ python/125.valid-palindrome/125.valid-palindrome.py
@@ -7,6 +7,26 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-# @lc code=end
+        low = s.lower()
 
+        l = 0
+        r = len(low) - 1
+
+        while l <= r:
+            
+            if not (ord("a") <= ord(low[l]) <= ord("z") or ord("0") <= ord(low[l]) <= ord("9")):
+                l += 1
+                continue
+
+            if not (ord("a") <= ord(low[r]) <= ord("z") or ord("0") <= ord(low[r]) <= ord("9")):
+                r -= 1
+                continue
+
+            if low[l] != low[r]:
+                return False
+
+            l += 1
+            r -= 1
+        
+        return True
+# @lc code=end
