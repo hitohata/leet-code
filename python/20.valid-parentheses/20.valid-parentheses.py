81ec5e165abb96cb11a16119d56a38476b8df0de
hirohata <43802663+hitohata@users.noreply.github.com>
2024-09-04 10:59:35 -0700

    20 (#27)
    * 36
    
    * 238
    
    * added 20
    
    * add

--- python/20.valid-parentheses/20.valid-parentheses.py
+++ python/20.valid-parentheses/20.valid-parentheses.py
@@ -7,6 +7,27 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-# @lc code=end
+        list_s = list(s)
+        stack = []
 
+        for s in list_s:
+            if s in ["(", "{", "["]:
+                stack.insert(0, s)
+                continue
+
+            if len(stack) == 0:
+                return False
+            
+            top = stack.pop(0)
+
+            if top == "(" and s == ")":
+                continue
+            if top == "{" and s == "}":
+                continue
+            if top == "[" and s == "]":
+                continue
+            
+            return False
+        
+        return True if len(stack) == 0 else False
+# @lc code=end
