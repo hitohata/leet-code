f9a7393056f30b8ad6937fa0af5edfa0170a03ad
hitohata <hirohatatro@gmail.com>
2025-05-09 22:24:47 -0700

    feat: update 70

--- rust/src/bin/70/70.climbing-stairs.rs
+++ rust/src/bin/70/70.climbing-stairs.rs
@@ -7,25 +7,19 @@
 // @lc code=start
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        let mut step = [1, 2];
+        let mut arr = vec![0; n as usize + 2];
+        arr[1] = 1;
+        arr[2] = 2;
 
-        if n < 2 {
-            return 1
+        if n <= 2 {
+            return n
         }
 
-        for i in 0..n {
-
-            if i < 2 {
-                continue;
-            }
-
-            let tmp = step[1];
-            step[1] += step[0];
-            step[0] = tmp;
-
+        for i in 3..=(n as usize)+1 {
+            arr[i] = arr[i - 1] + arr[i - 2]; 
         }
 
-        step[1]
+        return arr[n as usize]
     }
 }
 // @lc code=end
